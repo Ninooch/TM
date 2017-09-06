8390253e2d33aa30d5559d4193d0c15c6bd18b79
Ninooch <ninachaton@gmail.com>
2017-09-06 17:12:38 +0200

    ajout des fonds pour preload et mainMenu

--- scripts/states/mainMenuState.js
+++ scripts/states/mainMenuState.js
@@ -1,5 +1,9 @@
 var mainMenuState = {
     create: function(){
-        //alert("pouet");
+        game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+        bg = this.add.sprite(0,0,"mainBg");
+        bg.width = 800;
+        bg.height = 450;
     }
 };
--- scripts/states/preloadState.js
+++ scripts/states/preloadState.js
@@ -1,22 +1,23 @@
 var preloadState = {
     preload: function(){
-        loadBg = this.add.sprite(0,0,"loadBg");
-        loadBar = this.add.sprite(40,324,"loadBar");
-        this.load.setPreloadSprite(loadBar);
-        
- 
-        
-    },
-    create: function(){
+        //fond 
         game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
-
+        loadBg = this.add.sprite(0,0,"loadBg");
         loadBg.width = 800;
         loadBg.height = 450;
-
+        
+        //barre de chargement
+        loadBar = this.add.sprite(40,324,"loadBar");
         loadBar.width = 720;
         loadBar.height = 40;
+        //this.load.setPreloadSprite(loadBar); // pour render la barre en même temps que les fichiers chargent, expérimental
+
+        //chargements 
+        game.load.image("mainBg","assets/sprites/main.png");
+
+    },
+    create: function(){
 
         game.state.start("mainMenu");
     }
-
 };
