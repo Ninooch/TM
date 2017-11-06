8e11fbf7ee937f853df312b2d9ff7b10e79eaf99
Ninooch <ninachaton@gmail.com>
2017-11-06 21:38:23 +0100

    test des callbacks réussi!! youpi!

--- webSrc/scripts/dialogsList.js
+++ webSrc/scripts/dialogsList.js
@@ -11,19 +11,19 @@ var callback1 = function(){
 }
 
 var callback2 = function(){
-    globls.pnjs.martin.currentIndex = 2;
+    globals.pnjs.martin.currentIndex = 2;
     globals.dialogManager.stop(true,false);
-    globals.dialogManager.startDialog(pnjTest);
+    globals.dialogManager.startDialog(globals.pnjs.martin);
 }
 
 
 function setDialog(langue){
     switch(langue){
         case 0 :
-            globals.dialogs.pnjTestDialogs = [["bonjour! je suis un test . tu peux choisir entre un et deux.",["nouvelle map","option deux"],[callback1,callback2]],"j'affirme que tu as bien choisi l'option une","j'affirme que tu as bien choisi l'option deux"];
+             globals.dialogs.pnjTestDialogs = [["bonjour! je suis un test . tu peux choisir entre un et deux.",["nouvelle map","option deux"],[callback1,callback2]],"j'affirme que tu as bien choisi l'option une","j'affirme que tu as bien choisi l'option deux"];
             break; // chaque réplique à question est stockée dans un array. à l'index 0 se trouve la question. à l'index 1, les réponses contenues dans un array. à l'index 2  les callbacks des réponses aussi dans un array.
         case 1:
-            globals.dialogs.pnjTestDialogs = ["hi! I'm a test. You can choose between one and two. kjfkjshdk hdf jkhsd kjh fskj sdfkjhdkh kjsdfh kjdsfh kjdsh kj fkdhj skjh ksj hhkfsjh ksdfjh kjdhf kjhfskjshf kjsfh kjh fkjhsf kjh fkjh fj fskjh fsdkjh sjh fdsjkh dkjh fjhkdkj fdskjh ","you chose option 1","you chose option 2"];
+             globals.dialogs.pnjTestDialogs = [["hi! I'm a test. You can choose between one and two. kjfkjshdk hdf jkhsd kjh fskj sdfkjhdkh kjsdfh kjdsfh kjdsh kj fkdhj skjh ksj hhkfsjh ksdfjh kjdhf kjhfskjshf kjsfh kjh fkjhsf kjh fkjh fj fskjh fsdkjh sjh fdsjkh dkjh fjhkdkj fdskjh ",function(){console.log("fonction 1")}],"you chose option 1","you chose option 2"];
             break;
                  }
 }
